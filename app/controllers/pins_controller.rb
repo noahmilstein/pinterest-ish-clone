203a995c15220f71b228f1d401929fda119c6053
noahmilstein <casanoah@gmail.com>
2016-12-23 18:20:15 -0500

    pins WIP

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,43 @@
+class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all.order('created_at DESC')
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new pin"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+end
---
+++ spec/factories/pins.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :pin do
+    title "MyString"
+    description "MyText"
+  end
+end
