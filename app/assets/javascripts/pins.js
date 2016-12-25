e54098685e0bea5de72beffb0c9865dd5636eaf7
noahmilstein <casanoah@gmail.com>
2016-12-25 15:55:22 -0500

    added masonry gem with transitions and styling

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// // coffeescript
+// $ ->
+//   $('#pins').imagesLoaded ->
+//     $('#pins').masonry
+//       itemSelector: '.box'
+//       ifFitWidth: true
+
+// javascript
+$(function() {
+  return $('#pins').imagesLoaded(function() {
+    return $('#pins').masonry({
+      itemSelector: '.box',
+      ifFitWidth: true
+    });
+  });
+});
